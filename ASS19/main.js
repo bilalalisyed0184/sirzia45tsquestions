9b8dd9751635f14ade5c1db380167e6dab8dea46
bilalalisyed0184 <156933641+bilalalisyed0184@users.noreply.github.com>
2024-07-05 00:23:51 -0700

    Add files via upload

---
+++ ASS19/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// ASSIGNMENT:19
+// 19.	Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+const main_1 = require("../ASS14/main");
+console.log(`PRINTING NUMBER OF INVITED GUEST`);
+console.log(`we have finally invited ${main_1.guest_List.length}`);
---
+++ ASS19/main.ts
@@ -0,0 +1,6 @@
+// ASSIGNMENT:19
+// 19.	Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+import{ guest_List } from'../ASS14/main'
+console.log(`PRINTING NUMBER OF INVITED GUEST`);
+console.log(`we have finally invited ${guest_List.length}`);
