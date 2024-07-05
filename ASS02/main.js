ccfa690e582033c9c7e5ce4221268c814dabe149
bilalalisyed0184 <156933641+bilalalisyed0184@users.noreply.github.com>
2024-07-05 00:15:06 -0700

    Add files via upload

---
+++ ASS02/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// ASSIGNMENT:2
+// QUESTION: Personal Message:Store a person's name in a variable,and print a message to that person. your message shuld be simple, such as, "Hello Eric, would you like to learn some Python today?"
+Object.defineProperty(exports, "__esModule", { value: true });
+let personName = "Eric";
+console.log(`Hello${personName} would you like to learn some Python today?`);
---
+++ ASS02/main.ts
@@ -0,0 +1,5 @@
+// ASSIGNMENT:2
+// QUESTION: Personal Message:Store a person's name in a variable,and print a message to that person. your message shuld be simple, such as, "Hello Eric, would you like to learn some Python today?"
+
+let personName : string = "Eric";
+console.log(`Hello${personName} would you like to learn some Python today?`);
