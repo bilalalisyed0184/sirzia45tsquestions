c80d85e60186a2ce394bb0ddc0fd46d1d797cb56
bilalalisyed0184 <156933641+bilalalisyed0184@users.noreply.github.com>
2024-07-05 00:25:03 -0700

    Add files via upload

---
+++ ASS20/main.ts
@@ -0,0 +1,8 @@
+// ASSIGNMENT:20
+// 20.	Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let languages : string[] = ["english" , "hindhi" ,"urdu"];
+console.log("list of countries:");
+for (let top of languages) {
+   console.log(top); 
+}
