5213933a900645af94e0868dc27ad42508843f72
bilalalisyed0184 <156933641+bilalalisyed0184@users.noreply.github.com>
2024-07-05 00:19:15 -0700

    Add files via upload

---
+++ ASS10/main1.ts
@@ -0,0 +1,6 @@
+// ASSIGNMENT:10
+// 10.	Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// my name is syed bilal ali shah
+// date: 3/6/2024
+// this line multiply 5 with two and show the result on the console
+console.log(5 * 2);
